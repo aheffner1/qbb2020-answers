94f43c5bfcc526afb03107105898ac09a5d612fe
aheffner1 <aheffne1@jhu.edu>
2020-09-03 21:10:30 -0400

    now prints the seq_id target_start query_start and kmer in that order

--- day4-hw/01-day4-hw.py
+++ day4-hw/01-day4-hw.py
@@ -7,6 +7,15 @@ target = FASTAReader(open('subset.fa'))
 
 query_kmers = {}
 k = 11     #kmer length
+#! /usr/bin/env python 3
+import sys
+
+from fasta_iterator_class import FASTAReader
+query =  FASTAReader(open('droYak2_seq.fa'))
+target = FASTAReader(open('subset.fa'))
+
+query_kmers = {}
+k = 11     #kmer length
 
 # 1. Read in query sequence 
 for seq_id, sequence in query:
@@ -24,20 +33,21 @@ for seq_id, sequence in target:
         kmer = sequence[i:i + k]
         
         target_kmers.setdefault(kmer, {})
-        target_kmers[kmer][seq_id] = i
+        target_kmers[kmer].setdefault(seq_id, [])
+        target_kmers[kmer][seq_id].append(i)
         
 #3. Loop through each query match key to any in target
-matching_kmers = {}
 for kmer in query_kmers:
-    if kmer in target_kmers:
-        # matching kmers - key = kmer value is list with start of query and start of targets
-        start_target = target_kmers[kmer]
-        start_query = query_kmers[kmer]
-        matching_kmers.setdefault(kmer, [])
-        matching_kmers[kmer] = [start_query, start_target]
-
-for key in matching_kmers:
-    print(key, matching_kmers[key])
-
-if
+    if kmer not in target_kmers:
+        continue
+# matching kmers - key = kmer value is list with start of query and start of targets
+    start_query = query_kmers[kmer]
     
+    for seq_id in target_kmers[kmer]:
+        start_target = target_kmers[kmer][seq_id]
+        print(seq_id, start_target, start_query, kmer)
+        
+    #target_sequence_name    target_start    query_start k-mer
+
+if __name__ == "__main__":
+    reader = FASTAReader(sys.stdin)
